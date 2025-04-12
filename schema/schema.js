8ab63fec65e2e4e54cb8cf107359edc8029fde99
anurag-corptec <anurag.chhabra@corptec.com.au>
2025-04-12 12:57:43 +0530

    graphql init

---
+++ schema/schema.js
@@ -0,0 +1,48 @@
+const graphQL = require("graphql");
+const axios = require("axios");
+
+const { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString } = graphQL;
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+  },
+});
+
+const UserType = new GraphQLObjectType({
+  name: "Users",
+  fields: {
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    company: {
+      type: CompanyType,
+    },
+  },
+});
+
+const rootQuery = new GraphQLObjectType({
+  name: "rootQuery",
+  fields: {
+    users: {
+      type: new graphQL.GraphQLList(UserType),
+      args: {
+        id: { type: GraphQLInt },
+        name: { type: GraphQLString },
+      },
+      async resolve(parentValue, args) {
+        const queryParams = new URLSearchParams(args).toString();
+        const url = queryParams
+          ? `http://localhost:3000/users?${queryParams}`
+          : `http://localhost:3000/users`;
+
+        return axios.get(url).then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: rootQuery,
+});
